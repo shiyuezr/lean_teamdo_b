e829f9520d4152de28b940c19d1ebbb51d0e7fb0
deepnobug <2575816470@qq.com>
2021-01-23 10:49:06 +0800

    fix: 缺失的command

---
+++ commands/syncdb.py
@@ -0,0 +1,16 @@
+﻿# -*- coding: utf-8 -*-
+
+import os
+import json
+import shutil
+
+from command_base import BaseCommand
+
+class Command(BaseCommand):
+	help = "syncdb"
+	args = ''
+	
+	def handle(self, **options):
+		cmd = "BEEGO_RUNMODE=dev go run commands/cmd.go orm syncdb -v"
+		print 'run> ', cmd
+		os.system(cmd)
